9391cfd4229da0fe30b071356d6e3c5978931e6e
Horsed <legendaryhorsed@gmail.com>
2019-04-04 15:30:45 +0000

    add script to publish nightly builds

---
+++ publish-nightly.bat
@@ -0,0 +1,15 @@
+@echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+
+del %HOMEPATH%\babun-nightly\babun*.zip
+
+copy target\babun-dist\babun*.zip %HOMEPATH%\babun-nightly
+
+cd %HOMEPATH%\babun-nightly
+
+ren babun-*.zip babun-*-%fullstamp%.zip
